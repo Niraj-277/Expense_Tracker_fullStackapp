0bd79cd55aa98d95ba50514fb34ad24802c55502
Niraj <nirajrampal217@gmail.com>
2026-02-05 22:26:43 +0530

    authentication

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,54 +1,96 @@
-const express= require('express')
-const User=require('../models/user')
+const express = require("express");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // @desc Register controller
 // @route POST api/v1/register
-exports.register=async (req,res,next) => {
-    try{
-
-        //1. getting the data from the request
-        const {name,email,password,}=req.body
-        if(!user){
-            res.status(400).json({
-                success:false,
-                error:"provide the body"
-            })
-        }
-        //2. creating the user in the database
-        const user = await User.create({
-            name,
-            email,
-            password
-        })
-
-    }catch(error){
-        res.status(400).json({
-            success:false,
-            error:error.message
-        })
+exports.register = async (req, res, next) => {
+  try {
+    //1. getting the data from the request
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
     
-}
+    //2. creating the user in the database
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    res.status(201).json({
+      success: true,
+      id: user._id,
+      email: user.email,
+      message: "user registered successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
 
 
+//@desc Login controller
+//@route POST api/v1/login
 
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-
-
-
-exports.loginController=async (req,res,next) => {
-    try{
-        const user= req.body 
-        if(!user){
-            res.status(400).json({
-                success:false,
-                error:'did not get the user'
-            })
-        }
-
-    }catch(error){
-
+    // 1. validation
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        error: "Please enter email and password",
+      });
     }
+
+    // 2. find user + include password
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+
+    // 3. compare password
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid credentials",
+      });
+    }
+
+    //send the token her
+    //jwt.sign(payload,secret key, options )
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // 4. success login
+    res.status(200).json({
+      success: true,
+      message: "Login successful (token will be sent)",
+      token,
+    });
     
-}
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -23,5 +23,27 @@ const userSchema=new mongoose.Schema({
         default:Date.now
     }
 })
+//1. Encrypt the password 
+
+userSchema.pre('save',async function (next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password= await bcrypt.hash(this.password,salt)
+    console.log(this.password)
+})
+
+//2. Match the user password 
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password)
+ 
+};
+
+
+
+
+
 const User=mongoose.model('User',userSchema)
 module.exports=User;
